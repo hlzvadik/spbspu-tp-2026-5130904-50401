32f33f5a43f9cc195a8d0ee0a55f8cff32733fac
hlzvadik <golcovv716@gmail.com>
2026-05-15 18:44:10 +0300

    Normal look

--- goltsov.vadim/T3/poligon_functions.hpp
+++ goltsov.vadim/T3/poligon_functions.hpp
@@ -9,4 +9,73 @@ namespace goltsov
   void count(std::istream& is, std::ostream& os, std::vector< Polygon >& all_polygons);
   void maxseq(std::istream& is, std::ostream& os, std::vector< Polygon >& all_polygons);
   void inframe(std::istream& is, std::ostream& os, std::vector< Polygon >& all_polygons);
+
+  struct EvenPredicate
+  {
+    bool operator()(const Polygon& p);
+  };
+
+  struct OddPredicate
+  {
+    bool operator()(const Polygon& p);
+  };
+
+  struct CountVertexesPredicate
+  {
+    size_t count_vertexes;
+    bool operator()(const Polygon& p);
+  };
+
+  struct CountVertexes
+  {
+    size_t operator()(const Polygon& p);
+  };
+
+  struct AreasTriangleOfPolygon
+  {
+    const std::vector< Point >& points;
+    double operator()(const Point& p1);
+  };
+
+  struct AreaPolygon
+  {
+    double operator()(const Polygon& p);
+  };
+
+  struct NowMax
+  {
+    size_t now;
+    const Polygon& templ;
+    size_t operator()(const Polygon& p);
+  };
+
+  struct GetX
+  {
+    int operator()(const Point& p);
+  };
+
+  struct GetY
+  {
+    int operator()(const Point& p);
+  };
+
+  struct GetMinX
+  {
+    int operator()(const Polygon& p);
+  };
+
+  struct GetMaxX
+  {
+    int operator()(const Polygon& p);
+  };
+
+  struct GetMinY
+  {
+    int operator()(const Polygon& p);
+  };
+
+  struct GetMaxY
+  {
+    int operator()(const Polygon& p);
+  };
 }
